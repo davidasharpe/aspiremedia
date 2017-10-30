4e848da6f93c39a04fafef92d4fa8f47d40b5217
David Sharpe <davidasharpe@me.com>
2017-10-30 21:34:05 +1100

    Added Gulp Dependencies

---
+++ gulp.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat');
+
+gulp.task('log', function(){
+
+
+});
+
